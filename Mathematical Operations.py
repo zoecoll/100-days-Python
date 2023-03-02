21a5c61e53b9db744152e5bbae49281fe298512e
zoecoll <107445862+zoecoll@users.noreply.github.com>
2023-03-02 06:59:09 -0600

    Update Mathematical Operations.py

--- Mathematical Operations.py
+++ Mathematical Operations.py
@@ -3,3 +3,15 @@ num_char = len(input ("What is your name?"))
 new_num_char = str(num_char)
 
 print("Your name has " + new_num_char + " characters.")
+
+####
+
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+result = int(two_digit_number[0]) + int(two_digit_number[1])
+print (result)
