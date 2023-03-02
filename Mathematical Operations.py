49c7639290d0eb4d633d4605898a1d7e0377928f
zoecoll <107445862+zoecoll@users.noreply.github.com>
2023-03-02 07:03:50 -0600

    Update Mathematical Operations.py

--- Mathematical Operations.py
+++ Mathematical Operations.py
@@ -15,3 +15,10 @@ two_digit_number = input("Type a two digit number: ")
 
 result = int(two_digit_number[0]) + int(two_digit_number[1])
 print (result)
+
+# OR
+
+num_one = int(two_digit_number[0])
+num_two = int(two_digit_number[1])
+result = num_one + num_two
+print (result)
